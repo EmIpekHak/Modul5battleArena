f29d846c5d77aa5fd3b8c7447e793f7f3838464e
Rafeler <rhackl@tsn.at>
2024-10-10 19:52:22 +0200

    Zwerge Klasse fertig erstellt

--- src/Zwerge.java
+++ src/Zwerge.java
@@ -7,11 +7,48 @@ public class Zwerge extends Kämpfer{
         random = new Random();
     }
 
+    @Override
+    public void spezialfaehigkeitAktivieren() {
+        if(!isSpezialfaehigkeitAktiv()&&getLebenspunkte()<50){
+            setSpezialfaehigkeitAktiv(true);
+            System.out.println("Hat die Spezialfähigkeit aktiviert.");
+        }
+    }
+
+    @Override
+    public void spezialfaehigkeitDeaktivieren() {
+        if(isSpezialfaehigkeitAktiv()){
+            setSpezialfaehigkeitAktiv(false);
+            System.out.println("Hat die Spezialfähigkeit deaktiviert.");
+        }
+    }
+
     public void angreifen(Kämpfer gegner) {
     int schaden = random.nextInt(11) + 15;
     if (isSpezialfaehigkeitAktiv()&& getLebenspunkte() < 50){
-        int erfolg = if (getLebenspunkte() <=10?7:getLebenspunkte() <= 20?5: getLebenspunkte() <= 50?3;
+        int erfolg = 0;
+        if (getLebenspunkte() <= 10) {
+            erfolg = (7);
+        } else {
+            if (getLebenspunkte() <= 20) {
+                erfolg = (5);
+            } else {
+                if (getLebenspunkte() <= 50) erfolg = 3;
+            }
+            if (random.nextInt(11) <= erfolg) {
+                schaden *=2;
+                System.out.print("Zwerg hat erfolgreich seinem Gegner eine Kopfnuss gegeben damit hat er: " + schaden + "hinzugefügt!");
+            }
+            else {
+                schaden /=2;
+                System.out.print("Zwerg hat die Schulter des gegners mit der Kopfnuss getroffen deshalbt hat er nur: " + schaden + "gemacht");
+            }
+
+        }
+        gegner.schadenNehmen(schaden);
+        System.out.print("Der Gegner hat:" + schaden + "genommen");
     }
+
 
     }
 
